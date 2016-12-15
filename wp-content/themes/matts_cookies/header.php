0b811b3b683951ba92341c18eccc54c6f64deb55
aleksandrnikolajcuk <deathstars111>
2016-12-15 13:33:27 +0200

    SHipping

--- wp-content/themes/matts_cookies/header.php
+++ wp-content/themes/matts_cookies/header.php
@@ -124,7 +124,18 @@
         <meta name="msapplication-TileColor" content="#FFFFFF">
         <meta name="msapplication-TileImage" content="<?= TEMPLATEURI ?>/faviconit/favicon-144.png">
         <meta name="msapplication-config" content="<?= TEMPLATEURI ?>/browserconfig.xml">
-        
+
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+                    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+                m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-88912889-1', 'auto');
+            ga('send', 'pageview');
+
+        </script>
+
     </head>
     <body data-action="<?php echo admin_url( 'admin-ajax.php' );?>">
 <?php if (is_page() || is_single() || is_singular() || is_404()) {
